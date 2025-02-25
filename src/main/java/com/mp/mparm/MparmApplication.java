d4e3a3a9c5beb6921960d4e2ce484432eed71dba
mateusteixeir <mateusteixeiradas@gmail.com>
2025-02-25 17:46:41 -0300

    first commit

---
+++ src/main/java/com/mp/mparm/MparmApplication.java
@@ -0,0 +1,13 @@
+package com.mp.mparm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MparmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MparmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mp/mparm/MparmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.mparm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MparmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
